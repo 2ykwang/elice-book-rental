f63050c7b74b3b0962d2495ddc7edf4e00b3bd5e
Yeonggwang <immutable000@gmail.com>
2021-11-17 04:43:50 +0900

    Rental, Review 테이블 추가

--- app/main/views.py
+++ app/main/views.py
@@ -1,5 +1,7 @@
 from . import main
 from ..models import Book
+from flask import render_template
+
 @main.route("/")
 def index():
-    return "Test Page"
+    return render_template("book_list.html")
--- app/models.py
+++ app/models.py
@@ -10,8 +10,7 @@ class Book(db.Model):
     book_name = db.Column(db.String(100), nullable=False)
     publisher = db.Column(db.String(64), nullable=False)
     author = db.Column(db.String(64), nullable=False)
-    publication_date = db.Column(
-        db.DateTime, default=datetime.utcnow, nullable=False)
+    publication_date = db.Column(db.DateTime, nullable=False)
     pages = db.Column(db.Integer, nullable=False)
     isbn = db.Column(db.BigInteger, nullable=False)
     description = db.Column(db.Text, nullable=False)
@@ -21,10 +20,31 @@ class Book(db.Model):
 
 class User(db.Model):
     __tablename__ = 'users'
-    
+
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), unique=True, nullable=False)
     email = db.Column(db.String(64), unique=True, nullable=False)
     password_hash = db.Column(db.String(128), nullable=False)
-    created = db.Column(db.DateTime(), default=datetime.utcnow)
-    last_login = db.Column(db.DateTime(), default=datetime.utcnow)
+    created = db.Column(db.DateTime, default=datetime.utcnow)
+    last_login = db.Column(db.DateTime, default=datetime.utcnow)
+
+
+class Rental(db.Model):
+    __tablename__ = 'rental'
+
+    id = db.Column(db.Integer, primary_key=True)
+    created = db.Column(db.DateTime, default=datetime.utcnow)
+    returned = db.Column(db.Boolean, nullable=False)
+    return_date = db.Column(db.DateTime)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+
+
+class Review(db.Model):
+    __tablename__ = 'review'
+
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.Text, nullable=False)
+    score = db.Column(db.Integer, nullable=False)
+    created = db.Column(db.DateTime, default=datetime.utcnow)
+    book_id = db.Column(db.Integer, db.ForeignKey('books.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
