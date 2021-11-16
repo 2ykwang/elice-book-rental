adfaceb5cdad3d087b1f318e00c5173bf80039a8
Yeonggwang Yang <csdp000@naver.com>
2021-11-17 01:08:38 +0900

    Add User model

--- app/main/errors.py
+++ app/main/errors.py
@@ -8,8 +8,8 @@ def forbidden(e):
 
 @main.app_errorhandler(403)
 def page_not_found(e):
-    return render_template('errors/404.html'), 404
+    return render_template('errors/403.html'), 403
 
 @main.app_errorhandler(500)
 def internal_server_error(e):
-    return render_template('errors/404.html'), 404
+    return render_template('errors/500.html'), 500
--- app/models.py
+++ app/models.py
@@ -1,18 +1,30 @@
 from datetime import datetime
+from enum import unique
 from . import db
 
 
 class Book(db.Model):
     __tablename__ = "books"
-    
+
     id = db.Column(db.Integer, primary_key=True)
     book_name = db.Column(db.String(100), nullable=False)
-    publisher = db.Column(db.String(50), nullable=False)
-    author = db.Column(db.String(30), nullable=False)
+    publisher = db.Column(db.String(64), nullable=False)
+    author = db.Column(db.String(64), nullable=False)
     publication_date = db.Column(
-        db.DateTime, default=datetime.utcnow(), nullable=False)
+        db.DateTime, default=datetime.utcnow, nullable=False)
     pages = db.Column(db.Integer, nullable=False)
     isbn = db.Column(db.BigInteger, nullable=False)
     description = db.Column(db.Text, nullable=False)
     link = db.Column(db.String(128), nullable=False)
     image_url = db.Column(db.String(150), nullable=False)
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True, nullable=False)
+    email = db.Column(db.String(64), unique=True, nullable=False)
+    password_hash = db.Column(db.String(128), nullable=False)
+    created = db.Column(db.DateTime(), default=datetime.utcnow)
+    last_login = db.Column(db.DateTime(), default=datetime.utcnow)
