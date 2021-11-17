8228a5b24d342a06fd3c9d17fef13368154b07c3
Yeonggwang <immutable000@gmail.com>
2021-11-18 05:44:43 +0900

    feat: 로그인 기능 구현

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -6,18 +6,30 @@ from wtforms.validators import DataRequired, EqualTo
 
 class RegisterForm(FlaskForm):
     # Todo: 한글이름, 영문이름 체크 정규표현식
-    name = StringField('userid', validators=[DataRequired("이름을 입력해주세요."), validators.Regexp(
-        regex='[가-힣a-zA-Z]+', message="이름은 한글과 영문만 가능합니다.")])
+    name = StringField('name', validators=[
+        DataRequired("이름을 입력해주세요."),
+        validators.Regexp(regex='[가-힣a-zA-Z]+', message="이름은 한글과 영문만 가능합니다.")])
 
-    email = StringField('username', validators=[
+    email = StringField('email', validators=[
         DataRequired("이메일을 입력해주세요."),
+        validators.Email("올바른 형식의 이메일을 입력해주세요.")])
+
+    # Todo: password check
+    password = PasswordField('password', validators=[
+        DataRequired("비밀번호를 입력해주세요."),
         validators.Length(
             min=10, message="패스워드는 최소 10자리 이상 영어 대소문자 숫자 특수문자 조합으로 입력해주세요."),
-        validators.Email("올바른 형식의 이메일을 입력해주세요.")])
-    
-    # Todo: password check
-    password = PasswordField('password', validators=[DataRequired(
-        "비밀번호를 입력해주세요."), EqualTo('re_password', "비밀번호는 동일해야 합니다.")])
+        EqualTo('re_password', "비밀번호는 동일해야 합니다.")])
 
     re_password = PasswordField('re_password', validators=[
-                                DataRequired("동일한 비밀번호를 입력해주세요.")])
+        DataRequired("동일한 비밀번호를 입력해주세요.")])
+
+
+class LoginForm(FlaskForm): 
+    email = StringField('email', validators=[
+        DataRequired("이메일을 입력해주세요."),
+        validators.Email("올바른 형식의 이메일을 입력해주세요.")])
+
+    password = PasswordField('password', validators=[
+        DataRequired("비밀번호를 입력해주세요.")])
+                             
--- app/auth/views.py
+++ app/auth/views.py
@@ -1,10 +1,10 @@
- 
+
 from flask import render_template, redirect, request, url_for, flash
 from flask_login import login_user, logout_user, login_required, current_user
 
 from . import auth
 from ..validation import password_valid_check
-from .forms import RegisterForm
+from .forms import RegisterForm, LoginForm
 from ..models import User
 from .. import db
 from werkzeug.security import generate_password_hash, check_password_hash
@@ -23,7 +23,7 @@ def register2():
 @auth.route('/register', methods=['GET', 'POST'])
 def register():
     if current_user.is_authenticated:
-        return redirect(url_for('.index'))
+        return redirect(url_for('main.index'))
 
     register_form = RegisterForm()
 
@@ -36,7 +36,7 @@ def register():
             if not password_valid_check(password):
                 flash(f"비밀번호 형식이 잘못되었습니다. 안전한 비밀번호를 입력해주세요.")
                 return render_template('auth/register.html', form=register_form)
-                 
+
             # 이메일 체크
             if db.session.query(User.email).filter_by(email=email).first() is not None:
                 flash(f"{email} 이미 사용 중인 아이디(이메일) 입니다")
@@ -46,7 +46,7 @@ def register():
             user.name = name
             user.email = email
             user.set_password(password)
-            
+
             db.session.add(user)
             db.session.commit()
 
@@ -61,4 +61,28 @@ def register():
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template('auth/login.html')
+    if current_user.is_authenticated: 
+        return redirect(url_for('main.index'))
+
+    login_form = LoginForm()
+
+    if request.method == 'POST':
+        if login_form.validate_on_submit():
+            email = login_form.email.data
+            password = login_form.password.data
+            user = User.query.filter_by(email=email).first()
+            if user is not None and user.check_password(password):
+                login_user(user) 
+                return redirect(url_for('main.index'))
+            else:
+                flash("아이디 또는 비밀번호를 확인해주세요.")
+        else:
+            for message in login_form.errors.values():  
+                flash(str(message[-1]))
+
+    return render_template('auth/login.html', form=login_form)
+
+@auth.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('main.index'))
