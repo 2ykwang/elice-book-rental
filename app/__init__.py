9dfa756707f8e7f5f78b5f6b8fc8800aee5b6a41
Yeonggwang <immutable000@gmail.com>
2021-11-17 19:43:15 +0900

    feat: jinja2 data filter 추가

--- app/__init__.py
+++ app/__init__.py
@@ -12,7 +12,17 @@ def create_app(config_name):
     app.config.from_object(get_config(config_name))
 
     db.init_app(app)
-
+    app.jinja_env.filters['datetime'] = format_datetime
+    
     from .main import main as main_bp
     app.register_blueprint(main_bp)
     return app
+
+
+def format_datetime(value, format=None):
+    if format is None: 
+        format = "%Y년 %m월 %d일"
+        formatted = value.strftime(format.encode('unicode-escape').decode()).encode().decode('unicode-escape')
+    else:
+        formatted = value.strftime(format.encode('unicode-escape').decode()).encode().decode('unicode-escape')
+    return formatted
