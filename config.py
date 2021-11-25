2f8a100b1ff3f6d45b996d9e48756b4c2b307996
Yeonggwang <immutable000@gmail.com>
2021-11-26 00:38:10 +0900

    fix: Test Config

--- config.py
+++ config.py
@@ -30,6 +30,7 @@ class DevelopmentConfig(Config):
 
 class TestingConfig(Config):
     TESTING = True
+    SERVER_NAME = "localhost:5000"
     SQLALCHEMY_DATABASE_URI = (
         os.environ.get("TEST_DATABASE_URL")
         or f"sqlite:///{os.path.join(basedir, 'data-test.sqlite')}"
