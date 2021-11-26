a3fdedfc387e9c6135cd5b746102c2bb5f60a17f
Yeonggwang <immutable000@gmail.com>
2021-11-26 23:55:27 +0900

    chore: docker 환경 설정

--- gunicorn.config.py
+++ gunicorn.config.py
@@ -3,11 +3,12 @@ import os
 
 # gunicorn - configuration
 wsgi_app = "run:app"
+
 bind = "0.0.0.0:8000"
+workers = multiprocessing.cpu_count() * 2 - 1
 # bind = f'{os.getenv("HOST")}:{os.getenv("PORT")}'
 
 # heroku 환경에서 worker 갯수가 많은상태로 mysql을 사용하면 max_connection 초과됨.
-# workers = multiprocessing.cpu_count() * 2
 
 # CMD gunicorn --check--config ./gunicorn.config.py
 # CMD gunicorn -c ./gunicorn.config.py
